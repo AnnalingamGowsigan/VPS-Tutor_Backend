2e9513aadafc401884e9d6084acb99eb6b97edfd
AnnalingamGowsigan <e18115@eng.pdn.ac.lk>
2024-06-23 18:56:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv").config({ path: "./config/.env" });
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: true }));
+
+// Load routes
+const historyTakingQestionBankRoutes = require("./routes/historyTakingQestionBankRoutes");
+app.use("/api/historyTakingQestionBank", historyTakingQestionBankRoutes);
+
+const historyTakingQestionsByStudentRoutes = require("./routes/historyTakingQestionsByStudentRoutes");
+app.use(
+  "/api/historyTakingQestionsByStudent",
+  historyTakingQestionsByStudentRoutes
+);
+
+const teacher = require("./routes/teacherRoutes");
+app.use("/api/teacher", teacher);
+
+const dentalComplaintCasesRoutes = require("./routes/dentalComplaintCasesRoutes");
+app.use("/api/dentalComplaintCases", dentalComplaintCasesRoutes);
+
+const teethDetailsRoutes = require("./routes/teethDetails");
+app.use("/api/teethDetails", teethDetailsRoutes);
+
+const examintionQuestionsRoutes = require("./routes/examinationQuestionsRoutes");
+app.use("/api/examintionQuestions", examintionQuestionsRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
