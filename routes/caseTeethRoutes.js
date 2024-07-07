00f41d1c74bad98c8f0bd8487ce525ed065dd2b5
AnnalingamGowsigan <e18115@eng.pdn.ac.lk>
2024-07-07 16:11:39 +0530

    Refactor teethDetailsRoutes.js for improved readability and error handling

--- routes/caseTeethRoutes.js
+++ routes/caseTeethRoutes.js
@@ -29,15 +29,36 @@ router.put("/store", async (req, res) => {
     });
   }
 });
-
 // Route to get tooth details from Firestore
 router.get("/get", async (req, res) => {
   try {
     // Reference to the specific document in the Firestore collection
-    const complaintTypeRef = db.collection(COLLECTION_NAME).doc("caseDetails");
+    const complaintTypereference = db
+      .collection(COLLECTION_NAME)
+      .doc("caseDetails");
+
+    // Retrieve the document
+    const document = await complaintTypereference.get();
+    console.log(document.data());
+
+    if (!document.exists) {
+      return res.status(404).json({ error: "No such document!" });
+    }
+
+    const { mainTypeName, complaintTypeName, caseId } = document.data();
+
+    // Reference to the document
+    const complaintTypeRef = db
+      .collection("dentalComplaintCases")
+      .doc(mainTypeName)
+      .collection(complaintTypeName)
+      .doc(caseId)
+      .collection("toothDetails")
+      .doc("teeth");
 
     // Retrieve the document
     const doc = await complaintTypeRef.get();
+    console.log(doc);
 
     if (!doc.exists) {
       return res.status(404).json({ error: "No such document!" });
@@ -49,11 +70,8 @@ router.get("/get", async (req, res) => {
       data: doc.data(),
     });
   } catch (error) {
-    console.error("Error getting tooth details:", error);
-    res.status(500).send({
-      message: "Failed to get tooth details.",
-      error: error.message,
-    });
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
